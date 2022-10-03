42f30048177e5c7bf268c44c079b829452be98f3
Kennedy <kennedy.lopes@ufersa.edu.br>
2022-10-03 13:03:06 -0300

    Lendo os arquivos das cidades

---
+++ bancoDeDados/_creditos.md
@@ -0,0 +1,3 @@
+http://www.consultaesic.cgu.gov.br/busca/dados/Lists/Pedido/Item/displayifs.aspx?List=0c839f31%2D47d7%2D4485%2Dab65%2Dab0cee9cf8fe&ID=1012693&Web=88cc5f44%2D8cfe%2D4964%2D8ff4%2D376b5ebb3bef
+
+http://www.consultaesic.cgu.gov.br/busca/dados/Lists/Pedido/Attachments/1012693/RESPOSTA_PEDIDO_Coordenadas_Sedes_Municipios.zip
---
+++ lerArquivos.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5571
+
+typedef float latitude;
+typedef float longitude;
+
+struct cidade {
+    unsigned int id;
+    char *estado;
+    char *cidade;
+};
+
+struct gps {
+    unsigned int id;
+    latitude la;
+    longitude lo;
+};
+
+cidade *getCidades(char *arquivo);
+gps *getGps(cidade *, char *, int);
+
+int main() {
+    char *arquivo1 = (char *)"bancoDeDados/legenda.txt";
+    getCidades(arquivo1);
+    char *arquivo2 = (char *)"bancoDeDados/coordenadas.csv";
+    getCidades(arquivo2);
+}
+
+cidade *getCidades(char *arquivo) {
+    FILE *f = fopen(arquivo, "r");
+    cidade *cidades = (cidade *)malloc(MAX * sizeof(cidade));
+    if (!f) {
+        perror("Arquivo não existe");
+        return NULL;
+    }
+    fscanf(f, "CODIGO MUNICIPIO;UF;NOME MUNICIPIO");
+    unsigned int cod;
+    char *uf;
+    char *cid;
+    int i = 0;
+    while (!feof(f)) {
+        uf = (char *)malloc(2 * sizeof(char));
+        cid = (char *)malloc(40 * sizeof(char));
+        fscanf(f, "%d %s", &cod, uf);
+        fgets(cid, 40 * sizeof(char), f);
+        printf("%d %s %s", cod, uf, cid);
+        cidades[i].id = cod;
+        cidades[i].estado = uf;
+        cidades[i].cidade = cid;
+        i = i + 1;
+    }
+    return cidades;
+}
+
+gps *getGps(cidade *cidades, char *localizacoes) {
+    FILE *f = fopen(localizacoes, "r");
+    gps *local = (gps *)malloc(MAX * sizeof(gps));
+    if (!f) {
+        perror("Arquivo não existe");
+        return NULL;
+    }
+    fscanf(f, "CODIGO MUNICIPIO;UF;NOME MUNICIPIO");
+    unsigned int cod;
+    latitude la;
+    longitude lo;
+    int i = 0;
+    while (!feof(f)) {
+        fscanf(f, "%u;%f;%f", &cod, &la, &lo);
+        printf("%u;%f;%f\n", cod, la, lo);
+        local[i].id = cod;
+        local[i].la = la;
+        local[i].lo = lo;
+        i = i + 1;
+    }
+
+    return local;
+}
