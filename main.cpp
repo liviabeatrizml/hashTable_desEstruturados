f407098a514b09f00943249cdce08539758392d5
Leonardo_Dantas <l.dantas1155@gmail.com>
2022-10-22 16:03:30 -0300

    Implementando testes de colisões e de remoção

--- main.cpp
+++ main.cpp
@@ -1,42 +1,32 @@
 #include "Hash.cpp"
 #include "lerArquivos.cpp"
 
-void teste01() {
-    dataItem *d = (dataItem *)malloc(sizeof(dataItem));
-    d->city.cidade = (char *)"Pau dos Ferros";
-    d->city.id = 340940;
-    d->city.estado = (char *)"RN";
-    d->key = 340940;
-    d->GPS.id = 340940;
-    d->GPS.la = -6.11;
-    d->GPS.lo = -38.2;
+void inserirTodos();
 
-    int res = multiplicacao(d);
-    hash H;
-    init(H);
+int main(){
+    inserirTodos();
 
-    inserir(H, d, divisao);
-
-    // Não faz sentido mudar a função Hash, mas coloquei aqui para mostrar que é possível
-    inserir(H, d, multiplicacao);
-    remover(H, d, multiplicacao);
-    inserir(H, d, multiplicacao);
-    dataItem *dt = buscar(H, 340940, multiplicacao);
+    return 0;
 }
 
 void inserirTodos(){
-    cidade *todasCidades = getCidades((char*)"bancoDeDados/legenda.txt");
-    gps *todoLocal = getGps((char*)"bancoDeDados/coordenadas.csv");
-    dataItem* d = getItens(todasCidades, todoLocal);
+    cidade *allCity = getCidades((char*)"bancoDeDadosTemp/legenda.txt");
+    gps *allLocal = getGps((char*)"bancoDeDadosTemp/coordenadas.csv");
+
+    dataItem* d = getItens(allCity, allLocal);
+
     hash H;
     init(H);
-    int colisao = 0;
-    for(int i=0; i<MAX; i++){
-        colisao += -1*inserir(H, d+i, multiplicacao);
-    }
-}
 
-int main() {
-    //teste01();
-    inserirTodos(); //Tentativa de inserção
+    int colisao = 0;
+
+    for(int i = 0; i < MAX; i++){
+        colisao += -1 * inserir(H, d+i, divisao);
+    }
+
+    // printf("Colisoes: %d", colisao);
+
+    remover(H, buscar(H, 110001, divisao), divisao);
+
+    buscar(H, 430781, divisao);
 }
